77b2bdfc875cd1c59eff41ccf648a93a0b4a9de9
Swayam Jadhav <swayamvjadhav2010@gmail.com>
2026-07-06 19:10:08 +0530

    About section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   );
 }
